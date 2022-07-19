a3405b339a89e64d0056edf68d4dcc41535e8b1b
ermz <eramirez4369@gmail.com>
2022-07-19 07:59:13 -0400

    Added additional functionality to TokenFarm contract

--- contracts/TokenFarm.sol
+++ contracts/TokenFarm.sol
@@ -7,8 +7,25 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract TokenFarm is Ownable {
     // mapping token address -> staker address -> amount
     mapping(address => mapping(address => uint256)) public stakingBalance;
-
+    mapping(address => uint256) public uniqueTokensStaked;
+    address[] public stakers;
     address[] public allowedTokens;
+    IERC20 public dappToken;
+
+    constructor(address _dappTokenAddress) public {
+        dappToken = IERC20(_dappTokenAddress);
+    }
+
+    function issueTokens() public onlyOwner {
+        for (
+            uint256 stakersIndex = 0;
+            stakersIndex < stakers.length;
+            stakersIndex++
+        ){
+            address recipient = stakers[stakersIndex];
+            uint256 userTotalValue = getUserTotalValue(recipient);
+        }
+    }
 
     function stakeTokens(uint256 _amount, address _token) public {
         // what tokens
@@ -16,7 +33,17 @@ contract TokenFarm is Ownable {
         require(_amount > 0, "Amount has to be more than 0");
         require(tokenIsAllowed(_token), "Token is currently not allowed");
         IERC20(_token).transferFrom(msg.sender, address(this), _amount);
+        updateUniqueTokensStaked(msg.sender, _token);
         stakingBalance[_token][msg.sender] = stakingBalance[_token][msg.sender] + _amount;
+        if (uniqueTokensStaked[msg.sender] == 1) {
+            stakers.push(msg.sender);
+        }
+    }
+
+    updateUniqueTokensStaked(address _user, address _token) internal {
+        if (stakingBalance[_token][_user] <= 0) {
+            uniqueTokensStaked[_user] = uniqueTokensStaked[_user] + 1;
+        }
     }
 
     function addAllowedTokens(address _token) public onlyOwner {
